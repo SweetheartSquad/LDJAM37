bef6434a052470962d2e9214c077888455172329
seleb <sean.s.leblanc@gmail.com>
2016-12-10 12:27:57 -0500

    player update
    -functions follow prototype pattern
    -px/py, vx/vy, ax/ay for position, velocity, acceleration

--- client/player.js
+++ client/player.js
@@ -1,27 +1,48 @@
-
 function Player(){
-	this.x = 0;
-	this.y = 0;
+	this.vx = 0;
+	this.vy = 0;
+	this.ax = 0;
+	this.ay = 0;
+
+	this.px = size.x/2;
+	this.py = size.y/2;
+	
+
 	this.points = [];
 	this.graphics = new PIXI.Graphics();
+};
 
-	this.update = function(){
-		this.graphics.position.x += 0.1;
-	};
+Player.prototype.update = function(){
+	// integrate velocity by acceleration
+	this.vx += this.ax;
+	this.vy += this.ay;
 
-	this.draw =  function(){
-		///this.clear();
-		// set a fill and line style
-		this.graphics.beginFill(0xFF3300);
-		this.graphics.lineStyle(10, 0xffd900, 1);
-		// d.graphics.e
-		this.graphics.moveTo(50,50);
-		this.graphics.lineTo(250, 50);
-		this.graphics.lineTo(100, 100);
-		this.graphics.lineTo(250, 220);
-		this.graphics.lineTo(50, 220);
-		this.graphics.lineTo(50, 50);
-		this.graphics.endFill();
-	}
+	// integrate position by velocity
+	this.px += this.vx;
+	this.py += this.vy;
 
-}
+	// update actual graphics
+	this.graphics.x = this.px;
+	this.graphics.y = this.py;
+
+	// reset acceleration for next frame
+	this.ax = 0;
+	this.ay = 0;
+};
+
+Player.prototype.draw = function(){
+	var g = this.graphics;
+
+	g.clear();
+	// set a fill and line style
+	g.beginFill(0xFF3300);
+	g.lineStyle(10, 0xffd900, 1);
+	// draw character
+	g.moveTo(50,50);
+	g.lineTo(250, 50);
+	g.lineTo(100, 100);
+	g.lineTo(250, 220);
+	g.lineTo(50, 220);
+	g.lineTo(50, 50);
+	g.endFill();
+};
